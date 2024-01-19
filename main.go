e3be5f586bddb30f677cbb9cc876304caf11e842
dntjd1097 <dntjd1097@gmail.com>
2024-01-20 00:40:51 +0900

    test

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/mkvone/transaction-monitor/pkg"
 )
 
 func main() {
+	fmt.Println("File hierarchy of the current directory:")
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		fmt.Println(path)
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error walking directory: %v", err)
+	}
+
 	// Define a flag for the configuration path
 	var configPath string
 	flag.StringVar(&configPath, "config-path", "./config.yml", "Path to configuration file")
