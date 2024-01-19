936d840ee2b8d29220d605443081caf9c3f346f4
dntjd1097 <dntjd1097@gmail.com>
2024-01-20 00:58:49 +0900

    docker

--- main.go
+++ main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/mkvone/transaction-monitor/pkg"
 )
 
 func main() {
-	fmt.Println("File hierarchy of the current directory:")
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		fmt.Println(path)
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("Error walking directory: %v", err)
-	}
 
 	// Define a flag for the configuration path
 	var configPath string
